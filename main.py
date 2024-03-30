5ec8600c12c3cb1966323792d87964a5b629ec22
GreatRaksin <greatraksin@icloud.com>
2024-03-30 15:46:02 +0300

    Adding game objects 🎁

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import pygame as pg
+import random as r
 
 pg.init()  # инициализация библиотеки
 
@@ -9,6 +10,16 @@ W = 1280
 H = 720
 FPS = 60
 clock = pg.time.Clock()
+
+
+# COLORS
+GRAY = (237, 237, 237)
+RED = (214, 32, 32)
+BROWN = (128, 97, 61)
+
+# Game Objects
+player = pg.Rect(W // 2, H - 30, 100, 20)  # x, y, width, height
+apple = pg.Rect(r.randint(40, W - 40), -50, 40, 40)
 
 
 # Screen Config
@@ -24,4 +35,10 @@ while not game_over:  # бесконечный цикл для работы иг
 		if event.type == pg.QUIT:
 			pg.quit()
 			sys.exit()
+
+	screen.fill(GRAY)  # зальем экран серым цветом
+	pg.draw.rect(screen, BROWN, player)  # рисую игрового персонажа
+	pg.draw.ellipse(screen, RED, apple)  # рисую яблоко
+
+	pg.display.update()  # обновление экрана игры
 
