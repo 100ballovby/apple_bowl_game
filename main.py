05d6eecded2f062b0f945c08f2039dd0a7cf3913
GreatRaksin <greatraksin@icloud.com>
2024-04-20 14:41:10 +0300

    Adding pause menu

--- main.py
+++ main.py
@@ -50,6 +50,7 @@ apple.x, apple.y = r.randint(40, W - 40), -50
 
 # Game loop
 game_over = False
+pause = False
 while not game_over:  # бесконечный цикл для работы игры
 	if miss >= 3:
 		game_over = True
@@ -61,41 +62,44 @@ while not game_over:  # бесконечный цикл для работы иг
 			sys.exit()
 
 	screen.blit(bg_image, (0, 0))  # вместо screen.fill
-
-	screen.blit(player_img, player)
-	screen.blit(apple_img, apple)
-
-	score_text = score_font.render(f'Score: {score}', True, (107, 237, 185))
-	screen.blit(score_text, (10, 70))  # расположить score_text в координатах 10;10
-	lives = 3 - miss
-	for i in range(lives):
-		heart_rect.x = 10 + (i * heart_rect.width)
-		heart_rect.y = 5
-		screen.blit(heart_img, heart_rect)
-
-
-	pg.display.update()  # обновление экрана игры
-
-	apple_catch = gl.opponent_motion(apple, object_speed, W, H, player)
-	if apple_catch == 'miss':
-		miss += 1
-	elif apple_catch == 'catch':
-		score += 10
-
-	if not auto:
-		keys = pg.key.get_pressed()  # отслеживаю нажатие кнопок
-		if keys[pg.K_LEFT]:
-			player_speed = -speed
-		elif keys[pg.K_RIGHT]:
-			player_speed = speed
-		else:
-			player_speed = 0
+	if pause:
+		pass
 	else:
-		if apple.x > player.x:
-			player.x += 5
-		elif apple.x < player.x:
-			player.x -= 5
 
-	gl.player_motion(player, player_speed, W)
+		screen.blit(player_img, player)
+		screen.blit(apple_img, apple)
+
+		score_text = score_font.render(f'Score: {score}', True, (107, 237, 185))
+		screen.blit(score_text, (10, 70))  # расположить score_text в координатах 10;10
+		lives = 3 - miss
+		for i in range(lives):
+			heart_rect.x = 10 + (i * heart_rect.width)
+			heart_rect.y = 5
+			screen.blit(heart_img, heart_rect)
+
+
+		pg.display.update()  # обновление экрана игры
+
+		apple_catch = gl.opponent_motion(apple, object_speed, W, H, player)
+		if apple_catch == 'miss':
+			miss += 1
+		elif apple_catch == 'catch':
+			score += 10
+
+		if not auto:
+			keys = pg.key.get_pressed()  # отслеживаю нажатие кнопок
+			if keys[pg.K_LEFT]:
+				player_speed = -speed
+			elif keys[pg.K_RIGHT]:
+				player_speed = speed
+			else:
+				player_speed = 0
+		else:
+			if apple.x > player.x:
+				player.x += 5
+			elif apple.x < player.x:
+				player.x -= 5
+
+		gl.player_motion(player, player_speed, W)
 
 
